3ac70494175980498ba8ed9ed332aaf32113c6ae
Roy Barina <roy_barina@hotmail.com>
2020-04-13 20:03:07 +0300

    Added compile-franz.py script

---
+++ compile-franz.py
@@ -0,0 +1,90 @@
+import sys
+import os.path
+from subprocess import check_output
+
+
+def franzify(inFile):
+    """ 
+    Creates a new file without the outer 'moz-document' block.
+
+    We will iterate over all lines and ignore the moz-document and the last closing curly brace
+    by saving current lines in a temp variable until we found a closing curly brace
+    if a closing curly brace has found, we will add the last line with closing brace we found to the
+    temp lines variable and write it out. save the new line with curly brace and keep going
+    until we found the next..
+    after we reach the end of the file we can just write the remaining temp lines as is
+    this way we ignoring the last line with curly brace
+    """
+
+    print("Processing..")
+
+    # define needed variables
+    target = "@-moz-document domain("
+    processed = False
+    tempFile = inFile + '.tmp'
+    tempLines = ''
+    lastBraceLine = ''
+
+    # open in file to read and target file to write to
+    with open(inFile, 'r') as readObj, open(tempFile, 'w') as writeObj:
+        # iterating over all lines in the file
+        for line in readObj:
+            # if the current line starts with the moz-document we'll skip it
+            if line.startswith(target):
+                # and mark that we modified the file
+                processed = True
+            else:
+                # if the current line ends with a curly brace (meaning the end of a block)
+                if line.lstrip().startswith('}'):
+                    # means that there's a new 'end of a block' and we need to
+                    # include the old one we found earlier (if not the first time we found)
+                    # and add all of the lines we kept before
+                    tempLines = lastBraceLine + tempLines
+
+                    # then we need to write it all as usual
+                    # at this point the output file is similar to the input file at this location
+                    # except for the line we ignore at the start
+                    writeObj.write(tempLines)
+
+                    # now we will keep the new 'end block' line until we found another one
+                    lastBraceLine = line
+
+                    # reset variables
+                    tempLines = ''
+                else:
+                    # here we simply save the current line in a temp variable
+                    tempLines += line
+
+        # writing all the remaining lines excluding last line with closing curly brace
+        writeObj.write(tempLines)
+
+    # we need to check if we made changes to the file
+    if processed:
+        # then we will replace it with the original
+        os.remove(inFile)
+        os.rename(tempFile, inFile)
+    else:
+        # or just remove the temp file as it is identical to the original at this point
+        os.remove(tempFile)
+
+    print("Done.")
+
+
+# checking the amount of arguments given (name of this script is also an argument)
+if len(sys.argv) > 1:
+    argFile = sys.argv[1]
+    if os.path.isfile(argFile):
+        # call the shell command to compile to given object
+        output = check_output("stylus " + argFile, shell=True).decode()
+        print(output)
+        # and check if the output contains a 'compiled' sub-string
+        # this way we know the compilation was success
+        if output.find("compiled") >= 0:
+            # and now we can franzify the compiled CSS
+            franzify(argFile)
+        else:
+            print("Couldn't compile styl file.")
+    else:
+        print("Not a valid file " + argFile)
+else:
+    print("No file given.")
